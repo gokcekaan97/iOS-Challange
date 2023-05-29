fafad9679cc376d2aa574be3de2a228fc660d820
Kaan GÖKÇEK <kaan.gokcek@halkbank.com.tr>
2023-05-29 17:42:12 +0300

    pod initials

--- iOS-Challange/iOS-Challange/ViewController.swift
+++ iOS-Challange/iOS-Challange/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Carbon
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,10 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view.
   }
 
+
+}
+
+struct Label: Component {
 
 }
 
