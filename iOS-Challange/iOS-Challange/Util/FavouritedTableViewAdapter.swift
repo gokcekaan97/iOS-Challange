95a68302f77f828a1824fb82ed08935c39338902
Kaan GÖKÇEK <kaan.gokcek@halkbank.com.tr>
2023-06-07 09:43:13 +0300

    swipe delete now asks you sure

--- iOS-Challange/iOS-Challange/Util/FavouritedTableViewAdapter.swift
+++ iOS-Challange/iOS-Challange/Util/FavouritedTableViewAdapter.swift
@@ -12,13 +12,36 @@ import UIKit
 class FavouriteTableViewAdapter: UITableViewAdapter {
   @Published var indexPath: Int
   init() {
+
     self.indexPath = -1
   }
   func tableView(_ tableView: UITableView,
                  commit editingStyle: UITableViewCell.EditingStyle,
                  forRowAt indexPath: IndexPath) {
     if editingStyle == .delete {
-      self.indexPath = indexPath.row
+      showPopup { action in
+        if action {
+          self.indexPath = indexPath.row
+        }
+      }
     }
+  }
+  func showPopup(completion: @escaping (Bool) -> Void) {
+    let alertController = UIAlertController(title: "Uyarı", message: "Silme işlemini gerçekleştirmek istiyor musunuz?", preferredStyle: .alert)
+    alertController.addAction(UIAlertAction(title: "Evet",
+                                            style: .default,
+                                            handler: { _ in
+      completion(true)
+    }))
+    alertController.addAction(UIAlertAction(title: "Hayır",
+                                            style: .default,
+                                            handler: { _ in
+      completion(false)
+    }))
+    if let currentViewController = UIApplication.shared.keyWindow?.rootViewController {
+      currentViewController.present(alertController,
+                                    animated: true,
+                                    completion: nil)
+        }
   }
 }
